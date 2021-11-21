d452ae8b5d23be29bd3e2affcf44f3459e8c61b0
Skvortsov <skvorzov.sv@gmail.com>
2021-11-21 19:01:27 +0200

    Homework 22.11.2021

--- src/main/java/com/java/hw7/DynamicIntArray.java
+++ src/main/java/com/java/hw7/DynamicIntArray.java
@@ -1,22 +1,25 @@
 package com.java.hw7;
-
+import java.util.Arrays;
 public class DynamicIntArray {
 
     /**
      * Array for storing numbers
      */
-    private int[] array;
+    private int[] array = new int[10];
 
     /**
      * Current size of the array
      */
-    private int size;
+    private int size = 0;
 
     /**
      * Method for adding element to the array
      * @param element represents number need to add
      */
     public void add(int element) {
+        if (size == array.length) {
+            array[size++] = element;
+        }
     }
 
     /**
@@ -25,7 +28,14 @@ public class DynamicIntArray {
      * @return true if successfully removed and false if the array doesn't contain such an element
      */
     public boolean remove(int element) {
-        return false;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] == element) {
+                array[i] = 0;
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 
     /**
@@ -35,7 +45,12 @@ public class DynamicIntArray {
      * @return true if successfully updated and false if the array doesn't contain such an element
      */
     public boolean update(int element, int index) {
-        return false;
+        if (index < 0 || index >= size) {
+            return false;
+        } else {
+            array[index] = element;
+            return true;
+        }
     }
 
     /**
@@ -44,7 +59,10 @@ public class DynamicIntArray {
      * @return an element at the position with appropriate index
      */
     public int getElement(int index) {
-        return -1;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("NoSuchElementException");
+        } else { return array[index];
+        }
     }
 
     /**
@@ -52,13 +70,13 @@ public class DynamicIntArray {
      * @return size of the array
      */
     public int getSize() {
-        return -1;
+        return size;
     }
 
     /**
      * Method for creating new array if previous is full and copying all element from an old array to a new
      */
     private void resize() {
+        array = Arrays.copyOf(array, size * 3 / 2 + 1);
     }
-
 }
